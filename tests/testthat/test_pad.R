20d175e63cd1d9cb31cb7b551b6c98251d3a0e72
EdwinTh <edwinthoen@hotmail.com>
2016-11-16 20:36:38 +0100

    performance impovement get_interval

--- tests/testthat/test_pad.R
+++ tests/testthat/test_pad.R
@@ -1,25 +1,3 @@
-
-date_seq <- function(pulse){
-  # only use a wide pulse to test year, all others less wide for performance
-  if(pulse == 'year') {
-    start_date <- as.POSIXlt(strftime('2005-01-01'))
-  } else {
-    start_date <- as.POSIXlt(strftime('2015-01-01'))
-  }
-
-  sequence <- seq(start_date,
-                  as.POSIXlt(strftime('2017-01-01')),
-                  by = pulse)
-
-  set.seed(12345)
-  if(length(sequence) > 100) {
-    sampled_dates <- sample(sequence, 100)
-  } else {
-    sampled_dates <- sample(sequence, length(sequence) / 2)
-  }
-  return(sampled_dates)
-}
-
 x_year  <- c(as.Date('2015-01-01'), as.Date('2018-01-01'))
 x_month <- seq(as.Date('2015-01-01'), as.Date('2015-06-01'), by ='month')[c(1,3,4,6)]
 x_day   <- seq(as.Date('2015-01-01'), as.Date('2015-02-01'), by = 'day') %>% sample(15) %>%
