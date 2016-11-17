36f86ca9d1030a6fa8148408a0bcef4fc50101bf
Barret Schloerke <schloerke@gmail.com>
2016-11-17 15:53:44 -0500

    moved the data script to scripts folder

---
+++ scripts/emergency_data.R
@@ -0,0 +1,14 @@
+# Download for inclusion = 2016-10-17 16:15:40 CEST
+
+library(dplyr)
+
+url <- "https://storage.googleapis.com/montco-stats/tzr.csv"
+
+emergency <- read.csv(url(urlLink), header = TRUE, stringsAsFactors = FALSE) %>%
+  as_data_frame %>%
+  select(-desc, -e) %>%
+  mutate(timeStamp = ymd_hms(timeStamp, tz='EST')) %>%
+  rename(time_stamp = timeStamp)
+
+# Be careful when rerunning, data will be updated.
+devtools::use_data(emergency)
