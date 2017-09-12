5ee8fb0a8800d0f58ae6a9e6942bb98fdd18097f
EdwinTh <edwinthoen@hotmail.com>
2017-09-12 07:59:25 +0200

    skeleton of pad_cust ready

--- R/pad_cust.R
+++ R/pad_cust.R
@@ -1,4 +1,3 @@
-
 #spanned <- span_time("20160707 00", 20160711, tz = "CET") %>%
 #  subset_span(list(hour = c(0, 9, 16)))
 #coffee %>% thicken_cust(x, )
@@ -16,10 +15,11 @@ pad_cust <- function(x,
 
   original_data_frame <- x
   x <- as.data.frame(x)
-  original_interval <- interval
 
   # we have to get the dt_var twice, first on the original data. If there are
   # NA values in it, we have to get it again on x with NA values filtered out.
+
+  ## This part should be refactored!
   dt_var_info_original <- get_dt_var_and_name(x, by)
   dt_var_name <- dt_var_info_original$dt_var_name
   x_NA_list <- check_for_NA_pad(x, dt_var_info_original$dt_var,
@@ -32,25 +32,21 @@ pad_cust <- function(x,
 
   check_dt_var_in_group(dt_var_name, group)
 
+  # TODO check if there are no values in dt_var, not in spanned.
+
   # TODO check if the spanned an the dt_var are of the same type.
 
-    # Because dt_var might be changed we need to adjust it in the df to join later
+  # Because dt_var might be changed we need to adjust it in the df to join later
   pos <- which(colnames(x) == dt_var_name)
   x[, pos] <- dt_var
 
   group_vars_un  <- group_unique_vars(x, group)
   spanned_groups <- pad_cust_group_span(spanned, group_vars_un)
 
-  if (!is.null(interval)) {
-    if (!is.null(start_val)) dt_var <- dt_var[dt_var >= start_val]
-    if (!is.null(end_val)) dt_var <- dt_var[dt_var <= end_val]
-    check_interval_validity(spanned$span, dt_var)
-  }
-
   colnames(x)[colnames(x) == dt_var_name] <- 'span'
 
   return_frame <- suppressMessages(
-    dplyr::left_join(spanned, x)
+    dplyr::left_join(spanned_groups, x)
   )
 
   colnames(return_frame)[colnames(return_frame) == 'span'] <- dt_var_name
@@ -61,10 +57,6 @@ pad_cust <- function(x,
                                      original_data_frame)
 
   return_frame <- set_to_original_type(return_frame, original_data_frame)
-
-  if (is.null(original_interval)) {
-    interval_message(interval)
-  }
 
   dplyr::bind_rows(return_frame, x_NA)
 }
@@ -79,9 +71,9 @@ group_unique_vars <- function(x, group) {
 
 pad_cust_group_span <- function(spanned, group_vars) {
   if (is.null(group)) {
-    data_frame(spanned)
+    data_frame(span = spanned)
   } else {
     spanned <- rep(spanned, nrow(group_vars))
-
+    ### TO DO complete this data frame
   }
 }
