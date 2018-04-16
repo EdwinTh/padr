90916064ab37cc8a9559457c26d8be61f5985351
Edwin Thoen <edwinthoen@hotmail.com>
2018-04-16 08:37:06 +0200

    Update thicken_join.R

--- R/thicken_join.R
+++ R/thicken_join.R
@@ -1,23 +1,23 @@
-library(padr)
-library(dplyr)
-library(lubridate)
-thicken_join <- function(df_x, 
-                       df_y, 
-                       interval, 
-                       join_type = c("inner", 
-                                     "left", 
-                                     "right", 
-                                     "full", 
-                                     "anti",
-                                     "semi"),
-                       additional_keys = NULL,
-                       by_x = NULL,
-                       by_y = NULL) {
-  df_x <- thicken(df_x, interval = interval, colname = "join_me", by = by_x)
-  df_y <- thicken(df_y, interval = interval, colname = "join_me", by = by_y)
-  fun_name <- paste0(match.arg(join_type), "_join")
-  join_keys <- c("join_me", additional_keys)
-  ret <- do.call(eval(fun_name), list(x = df_x, y = df_y, by = join_keys))
-  select(ret, -join_me)
-}
+# library(padr)
+# library(dplyr)
+#library(lubridate)
+#thicken_join <- function(df_x, 
+#                       df_y, 
+#                       interval, 
+#                       join_type = c("inner", 
+#                                     "left", 
+#                                     "right", 
+#                                     "full", 
+#                                     "anti",
+#                                     "semi"),
+#                       additional_keys = NULL,
+#                       by_x = NULL,
+#                       by_y = NULL) {
+#  df_x <- thicken(df_x, interval = interval, colname = "join_me", by = by_x)
+#  df_y <- thicken(df_y, interval = interval, colname = "join_me", by = by_y)
+#  fun_name <- paste0(match.arg(join_type), "_join")
+#  join_keys <- c("join_me", additional_keys)
+#  ret <- do.call(eval(fun_name), list(x = df_x, y = df_y, by = join_keys))
+#  select(ret, -join_me)
+#}
 
